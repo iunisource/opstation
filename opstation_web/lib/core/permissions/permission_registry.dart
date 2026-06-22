01c8019ce713015e7efb66b07ce18008696ddd9f
Hamza Abbas <iunisource@gmail.com>
2026-06-22 13:08:18 +0500

    Add Opening Journal to permission registry (Financials)

--- opstation_web/lib/core/permissions/permission_registry.dart
+++ opstation_web/lib/core/permissions/permission_registry.dart
@@ -105,6 +105,7 @@ const List<PermModule> kPermissionRegistry = [
   PermModule('financial_reporting', 'Financials', Icons.account_balance_outlined, [
     PermItem('chart_of_accounts', 'Chart of Accounts', PermKind.doc, '/erp/chart-of-accounts'),
     PermItem('jv', 'Journal Vouchers', PermKind.doc, '/financials/journal-vouchers'),
+    PermItem('opening_journal', 'Opening Journal', PermKind.doc, '/financials/opening-journal'),
     PermItem('cpv', 'Payment Vouchers', PermKind.doc, '/erp/payment-vouchers'),
     PermItem('crv', 'Receipt Vouchers', PermKind.doc, '/erp/receipt-vouchers'),
     PermItem('pdc', 'PDC Voucher', PermKind.doc, '/erp/pdc-voucher'),
